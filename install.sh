b7931cf389ff93397e1c27f47e915f4bf0503c5e
tim-ts-chu <tim.ts.chu@gmail.com>
2019-02-15 12:04:15 +0000

    fix install.sh bug

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # install dependancy
 cd ${HOME}/linux-config
 if [ $? -ne 0 ]; then
-    echo "Change directory to linux-config failed."
+    echo "Change directory to ${HOME}/linux-config failed."
     echo "Please, make sure that linux-config is under the home directory."
     exit -1
 fi
@@ -10,6 +10,6 @@ fi
 # run all setup shell script
 for f in ~/linux-config/*setup.sh; do
 	echo $f;
-	#$f;
+	$f;
 done
 
