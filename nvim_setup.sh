af19d48ae76e143d29c0d3c913eaecad0bda48db
tim-ts-chu <tim.ts.chu@gmail.com>
2020-08-26 21:47:15 -0400

    build nvim and dependencies

--- nvim_setup.sh
+++ nvim_setup.sh
@@ -2,6 +2,36 @@
 
 BASEDIR=`realpath ~/linux-config`
 
+apt install -y wget tmux build-essential curl git
+
+# neovim build
+apt install -y ninja-build libtool-bin gperf luajit luarocks libuv1-dev libluajit-5.1-dev libunibilium-dev libmsgpack-dev libtermkey-dev libvterm-dev libutf8proc-dev gettext libtool autoconf automake cmake g++ pkg-config unzip
+cd ~; git clone -b v0.4.4 https://github.com/neovim/neovim.git
+cd ~/neovim; make CMAKE_BUILD_TYPE=Release
+cd ~/neovim; make install
+rm -rf ~/neovim
+
+# nodejs
+curl -sL https://deb.nodesource.com/setup_14.x > ~/install.sh
+bash ~/install.sh -y
+rm -rf ~/install.sh
+apt install -y nodejs cmdtest
+
+# yarn
+curl -sL https://yarnpkg.com/install.sh  > ~/install.sh
+bash ~/install.sh
+rm -rf ~/install.sh
+
+# coc dependency
+apt install -y python3-pip
+pip3 install pynvim jedi
+curl -sL install-node.now.sh/lts > ~/install.sh
+bash ~/install.sh -y
+rm -rf ~/install.sh
+
+apt install -y ranger
+pip3 install ranger-fm
+
 # create .config folder if not exist
 mkdir -p $HOME/.config
 
@@ -18,11 +48,13 @@ fi
 echo "link ${HOME}/.config/nvim from ${BASEDIR}/nvim"
 ln -s ${BASEDIR}/nvim ${HOME}/.config/nvim
 
-
-# install vim-plugin
+# vim-plug
 echo "install vim-plug"
-curl -fLo $HOME/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +PlugInstall +qall
+RUN curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+RUN nvim +PlugInstall +qall
 
+# coc package
+echo "coc package"
+RUN nvim -c 'CocInstall coc-snippets coc-json coc-tsserver'
 
 
