974ad978e15f63199c17747fcc579d41f4ab307e
tim-ts-chu <tim.ts.chu@gmail.com>
2020-08-29 15:52:32 -0400

    merge script

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 
-
-# install dependancy
+# check repo dir
 cd ${HOME}/linux-config
 if [ $? -ne 0 ]; then
     echo "Change directory to ${HOME}/linux-config failed."
@@ -21,11 +20,40 @@ $SUDO apt update
 $SUDO apt install -y wget tmux build-essential curl git
 
 # coc
-$SUDO apt install -y nodejs cmdtest python3-pip ranger nodejs cmdtest
+$SUDO apt install -y nodejs cmdtest python3-pip ranger
+
+#RUN apt update
+#RUN apt install -y wget git curl
+#RUN apt install -y nodejs cmdtest python3-pip ranger
+cd ~; wget https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
+chmod u+x ~/nvim.appimage
+cd ~; ~/nvim.appimage --appimage-extract
+mv ~/squashfs-root ~/.squashfs-root
+$SUDO ln -s ~/.squashfs-root/usr/bin/nvim /usr/bin/nvim
+rm -rf ~/nvim.appimage
+
+curl -sL https://yarnpkg.com/install.sh  > ~/install.sh
+bash ~/install.sh
+rm -rf ~/install.sh
+
+pip3 install pynvim jedi ranger-fm
+curl -sL install-node.now.sh/lts > ~/install.sh
+bash ~/install.sh -y
+rm -rf ~/install.sh
+
+# ignore conda first
+
+# linux-config
+cd ~; git clone -b master https://github.com/tim-ts-chu/linux-config.git
+mkdir -p ~/.config
+ln -s ~/linux-config/nvim ${HOME}/.config/nvim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
 
 # run all setup shell script
-for f in ~/linux-config/*setup.sh; do
-	echo $f;
-	$f;
-done
+#for f in ~/linux-config/*setup.sh; do
+#	echo $f;
+#	$f;
+#done
+
 
