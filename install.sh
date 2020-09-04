99f7cc7a253d71e00d6ee8323477179ca83d0689
tim-ts-chu <tim.ts.chu@gmail.com>
2020-09-04 18:10:31 +0000

    fix nvim

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ conda init
 rm -rf ~/miniconda.sh
 
 echo "===== coc dependencies ====="
-$SUDO apt install -y cmdtest ranger
+$SUDO apt install -y cmdtest
 
 echo "===== install binaries ====="
 mkdir -p ~/bin
@@ -36,16 +36,17 @@ cd ~; wget https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appima
 chmod u+x ~/nvim.appimage
 cd ~; ~/nvim.appimage --appimage-extract
 mv ~/squashfs-root ~/bin/.squashfs-root
-ln -s ~/bin/.squashfs/usr/bin/nvim ~/bin/nvim
+ln -s ~/bin/.squashfs-root/usr/bin/nvim ~/bin/nvim
 rm -rf ~/nvim.appimage
+
+#conda install -y nodejs pip
+conda install -y nodejs pip
+pip install pynvim jedi ranger-fm
 
 curl -sL https://yarnpkg.com/install.sh  > ~/install.sh
 bash ~/install.sh
 rm -rf ~/install.sh
 . ~/.bashrc
-
-conda install -y nodejs pip
-pip install pynvim jedi ranger-fm
 
 echo "===== vim plugin install ====="
 mkdir -p ~/.config
